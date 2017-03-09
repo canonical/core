bc7cf95d1ee98a7154d6215ea2121a0da2c146ff
Michael Vogt <mvo@ubuntu.com>
2017-03-09 12:24:24 +0100

    mock systemctl as well

--- tests/test_hook_pi_config.py
+++ tests/test_hook_pi_config.py
@@ -23,6 +23,9 @@ class TestPiConfigFromConfigureHook(unittest.TestCase):
     def setUp(self):
         self.tmp = tempfile.mkdtemp()
         self.addCleanup(shutil.rmtree, self.tmp)
+        # mock systemctl as well as this is checked from the configure
+        # hook and may not be available everyhwere
+        self.mock_binary("systemctl", "")
 
     def mock_uboot_config(self, txt):
         self.mock_uboot_config = os.path.join(self.tmp, "config.txt")
@@ -30,21 +33,22 @@ class TestPiConfigFromConfigureHook(unittest.TestCase):
             fp.write(txt)
         os.environ["TEST_UBOOT_CONFIG"]=self.mock_uboot_config
 
-    def mock_snapctl(self, k, v):
-        mock_snapctl=os.path.join(self.tmp, "snapctl")
+    def mock_binary(self, basename, script):
+        mocked_binary=os.path.join(self.tmp, basename)
         if not self.tmp in os.environ["PATH"]:
             os.environ["PATH"] = self.tmp+":"+os.environ["PATH"]
-        with open(mock_snapctl, "w") as fp:
-            fp.write("""#!/bin/sh
-if [ "$1" = "get" ] && [ "$2" = "%s" ]; then echo "%s"; fi
-            """ % (k, v))
-        os.chmod(mock_snapctl, 0o755)
+        with open(mocked_binary, "w") as fp:
+            fp.write("#!/bin/sh\n%s\n" % script)
+        os.chmod(mocked_binary, 0o755)
+
+    def mock_snapctl(self, k, v):
+        self.mock_binary("snapctl", """if [ "$1" = "get" ] && [ "$2" = "%s" ]; then echo "%s"; fi""" % (k, v))
 
     def read_mock_uboot_config(self):
         with open(self.mock_uboot_config) as fp:
             content=fp.read()
         return content
-            
+
     def test_configure_pi_config_uncomment_existing(self):
         self.mock_uboot_config(mock_config_txt)
         self.mock_snapctl("pi-config.disable-overscan", "1")
@@ -58,7 +62,7 @@ if [ "$1" = "get" ] && [ "$2" = "%s" ]; then echo "%s"; fi
         expected=mock_config_txt+"\n#avoid_warnings=1\n"
         subprocess.check_call(["hooks/configure"])
         self.assertEqual(self.read_mock_uboot_config(), expected)
-        
+
     def test_configure_pi_config_add_new_option(self):
         self.mock_uboot_config(mock_config_txt)
         self.mock_snapctl("pi-config.framebuffer-depth", "16")
