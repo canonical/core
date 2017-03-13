091e6b889f7323d0fe15c6593526d7ccce8d348b
Michael Vogt <mvo@ubuntu.com>
2017-03-13 15:45:43 +0100

    test writing 00-snap-core.conf

--- tests/test_hook_powerbtn.py
+++ tests/test_hook_powerbtn.py
@@ -10,6 +10,11 @@ class TestConfigurePowerkey(HookTest):
         os.environ["TEST_LOGIN_CONF_D"]=self.tmp
         self.mock_snapctl("system.power-key-action", "ignore")
         subprocess.check_call(["hooks/configure"])
+        with open(os.path.join(self.tmp, "00-snap-core.conf")) as fp:
+            content=fp.read()
+        self.assertEqual(content, """[Login]
+HandlePowerKey=ignore
+""")
 
     def test_configure_powerkey_invalid(self):
         self.mock_snapctl("system.power-key-action", "invalid")
