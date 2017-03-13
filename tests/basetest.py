851c6d9a94437a5a906913146e0f9318d765af0d
Michael Vogt <mvo@ubuntu.com>
2017-03-13 15:42:04 +0100

    add proper tests around service start

--- tests/basetest.py
+++ tests/basetest.py
@@ -4,21 +4,44 @@ import tempfile
 import unittest
 
 
+class MockCmd:
+    def __init__(self, tmpdir, basename, script):
+        self.mocked_binary=os.path.join(tmpdir, basename)
+        self.logfile = self.mocked_binary+".log"
+        script="""#!/bin/sh
+echo "$(basename "$0")" >> "{0}"
+for arg in "$@"; do
+    echo "$arg" >> "{0}"
+done
+echo >> "{0}"
+{1}
+        """.format(self.logfile, script)
+        if not tmpdir in os.environ["PATH"]:
+            os.environ["PATH"] = tmpdir+":"+os.environ["PATH"]
+        with open(self.mocked_binary, "w") as fp:
+            fp.write(script)
+        os.chmod(self.mocked_binary, 0o755)
+    def calls(self):
+        with open(self.logfile) as fp:
+            content = fp.read()
+        all_calls = []
+        for call in content.split("\n\n"):
+            if call:
+                all_calls.append(call.split("\n"))
+        return all_calls
+
+
 class HookTest(unittest.TestCase):
+
     def setUp(self):
         self.tmp = tempfile.mkdtemp()
         self.addCleanup(shutil.rmtree, self.tmp)
         # mock systemctl as well as this is checked from the configure
         # hook and may not be available everyhwere
-        self.mock_binary("systemctl", "")
+        self.mock_systemctl = self.mock_binary("systemctl", "")
 
     def mock_binary(self, basename, script):
-        mocked_binary=os.path.join(self.tmp, basename)
-        if not self.tmp in os.environ["PATH"]:
-            os.environ["PATH"] = self.tmp+":"+os.environ["PATH"]
-        with open(mocked_binary, "w") as fp:
-            fp.write("#!/bin/sh\n%s\n" % script)
-        os.chmod(mocked_binary, 0o755)
+        return MockCmd(self.tmp, basename, script)
 
     def mock_snapctl(self, k, v):
         self.mock_binary("snapctl", """if [ "$1" = "get" ] && [ "$2" = "%s" ]; then echo "%s"; fi""" % (k, v))
--- tests/test_hook_services.py
+++ tests/test_hook_services.py
@@ -5,13 +5,34 @@ from .basetest import HookTest
 
 class TestConfigureHookServices(HookTest):
 
+    def setUp(self):
+        super(TestConfigureHookServices, self).setUp()
+        
+        self.mock_systemctl = self.mock_binary("systemctl", """
+if [ "$1" = "is-enabled" ] && [ "$2" = "ssh.service" ]; then echo "disabled";exit 1; fi
+if [ "$1" = "is-active" ] && [ "$2" = "ssh.service" ]; then echo "inactive";exit 1; fi
+        """)
+
+    
     def test_configure_service_disable(self):
         self.mock_snapctl("service.ssh.disable", "true")
         subprocess.check_call(["hooks/configure"])
+        self.assertEqual(self.mock_systemctl.calls(), [
+            ["systemctl", "--version"],
+            ["systemctl", "disable", "ssh.service"],
+            ["systemctl", "stop", "ssh.service"],
+        ])
 
     def test_configure_service_enable(self):
         self.mock_snapctl("service.ssh.disable", "false")
         subprocess.check_call(["hooks/configure"])
+        self.assertEqual(self.mock_systemctl.calls(), [
+            ["systemctl", "--version"],
+            ["systemctl", "is-enabled", "ssh.service"],
+            ["systemctl", "enable", "ssh.service"],
+            ["systemctl", "is-active", "ssh.service"],
+            ["systemctl", "start", "ssh.service"],
+        ])
 
     def test_configure_service_invalid(self):
         self.mock_snapctl("service.ssh.disable", "invalid")
