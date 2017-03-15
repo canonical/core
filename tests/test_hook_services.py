a379b401a323b23a31d37d5f15e532967d8f4813
John R. Lenton <jlenton@gmail.com>
2017-03-15 16:56:38 +0000

    address review issues

--- tests/test_hook_services.py
+++ tests/test_hook_services.py
@@ -9,8 +9,8 @@ class TestConfigureHookServices(HookTest):
         super(TestConfigureHookServices, self).setUp()
         
         self.mock_systemctl = self.mock_binary("systemctl", """
-if [ "$1" = "is-enabled" ] && [ "$2" = "ssh.service" ]; then echo "disabled";exit 1; fi
-if [ "$1" = "is-active" ] && [ "$2" = "ssh.service" ]; then echo "inactive";exit 1; fi
+if [ "$1" = "is-enabled" ] && [ "$3" = "ssh.service" ]; then echo "disabled";exit 1; fi
+if [ "$1" = "is-active" ] && [ "$3" = "ssh.service" ]; then echo "inactive";exit 1; fi
         """)
 
     
@@ -28,9 +28,9 @@ if [ "$1" = "is-active" ] && [ "$2" = "ssh.service" ]; then echo "inactive";exit
         subprocess.check_call(["hooks/configure"])
         self.assertEqual(self.mock_systemctl.calls(), [
             ["systemctl", "--version"],
-            ["systemctl", "is-enabled", "ssh.service"],
+            ["systemctl", "is-enabled", "--quiet", "ssh.service"],
             ["systemctl", "enable", "ssh.service"],
-            ["systemctl", "is-active", "ssh.service"],
+            ["systemctl", "is-active", "--quiet", "ssh.service"],
             ["systemctl", "start", "ssh.service"],
         ])
 
