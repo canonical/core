c9ec86efc8ca5e0378fff4e7438d4f39695a7a48
Michael Vogt <mvo@ubuntu.com>
2018-11-20 12:13:30 +0100

    snapcraft: build fontconfig from the archive deb source

---
+++ fc/build-from-security.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+
+import apt
+import atexit
+import glob
+import os
+import shutil
+import subprocess
+import sys
+import tempfile
+
+
+def fetch_source_from_security(release, pkgname):
+    apt.apt_pkg.config.set("Acquire::AllowInsecureRepositories", "1")
+    with tempfile.TemporaryDirectory(prefix="aptroot-{}".format(release)) as base:
+        sources_list = base+"/etc/apt/sources.list"
+        os.makedirs(os.path.dirname(sources_list), exist_ok=True)
+        with open(sources_list, "w") as fp:
+            fp.write("""
+deb http://archive.ubuntu.com/ubuntu/ {dist} main
+deb-src http://archive.ubuntu.com/ubuntu/ {dist} main
+deb http://archive.ubuntu.com/ubuntu/ {dist}-security main
+deb-src http://archive.ubuntu.com/ubuntu/ {dist}-security main
+        """.format(dist=release))
+
+        cache = apt.Cache(rootdir=base)
+        cache.update(apt.progress.text.AcquireProgress())
+        cache.open()
+        # extract source
+        pkg_src_dir = tempfile.mkdtemp(prefix="{}-{}-src".format(release, pkgname))
+        os.makedirs(pkg_src_dir, exist_ok=True)
+        pkg = cache["fontconfig"]
+        if not glob.glob("{}/{}-*".format(pkg_src_dir, pkgname)):
+            pkg.candidate.fetch_source(destdir=pkg_src_dir)
+    return pkg_src_dir
+
+
+def build_fontconfig(release):
+    pkgbasesrcdir = fetch_source_from_security(release, "fontconfig")
+    atexit.register(shutil.rmtree, pkgbasesrcdir)
+    pkgsrcdir = glob.glob(pkgbasesrcdir+"/fontconfig-*")[0]
+    # apply any build fixes
+    for p in glob.glob("patches/{}/*.patch".format(release)):
+        print("applying {}".format(p))
+        if not os.path.exists(pkgsrcdir+"/debian/patches/"+p):
+            shutil.copy(p, os.path.join(pkgsrcdir, "debian", "patches"))
+            subprocess.call("echo {} >> {}/debian/patches/series".format(os.path.basename(p), pkgsrcdir), shell=True)
+    # do the normal build first
+    subprocess.check_call(["sudo", "apt-get", "install", "-y", "build-essential"])
+    subprocess.check_call(["sudo", "apt-get", "-y", "build-dep", pkgsrcdir])
+    subprocess.check_call(["dpkg-buildpackage", "-uc"], cwd=pkgsrcdir)
+    triplet=subprocess.check_output(["dpkg-architecture", "-qDEB_HOST_GNU_TYPE"]).decode().strip()
+    # then use this to get the static build
+    subprocess.check_call("../libtool  --tag=CC   --mode=link gcc   -g -O2 -pthread   -o fc-cache fc-cache.o ../src/.libs/libfontconfig.a /usr/lib/{triplet}/libfreetype.a /usr/lib/{triplet}/libexpat.a /usr/lib/{triplet}/libpng.a -lz -lm".format(triplet=triplet), shell=True, cwd=os.path.join(pkgsrcdir, "fc-cache"))
+    target_fc_cache="./fc-cache-{}".format(release)
+    shutil.copy(os.path.join(pkgsrcdir, "fc-cache/fc-cache"), target_fc_cache)
+    return target_fc_cache
+    
+
+if __name__ == "__main__":
+    release = sys.argv[1]
+    build_fontconfig(release)
+    
